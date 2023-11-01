c8b943cc3000d8df26a630d6adca34384e83d84b
ApoDarkMatter <ndrpln@gmail.com>
2023-11-01 12:12:11 +0100

    added login structure with protected routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React from "react";
+import ProtectedRoutes from "./middlewares/ProtectedRoutes";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Administration from './pages/administation/administration';
+import Login from "./pages/login/login";
 import './App.css';
+
 
 function App() {
   return (
-    <div>Ciao</div>
+    <Router>
+    <Routes>
+      <Route exact path="/" element={<Login />} />
+      <Route element={<ProtectedRoutes />}>
+          <Route path="/administration" element={<Administration />} />
+      </Route>
+    </Routes>
+  </Router>
   );
 }
 
---
+++ src/pages/administation/administration.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Administration() {
+  return (
+    <div>Login Successfully - This is Administration Area!</div>
+  )
+}
+
+export default Administration
